fde3c7f2f018ec265884379c12293c6ff866798e
BenJamin McMullin <benja@PSE2022-01.pseutah.com>
2025-04-07 17:51:48 -0600

    gitignore

---
+++ scripts/config.py
@@ -0,0 +1,5 @@
+# API keys & settings
+
+FRED_API_KEY = "b4891de85d7c95f74741f324c730d675"
+
+TREASURY_API_URL = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/accounting/od/avg_interest_rates"
